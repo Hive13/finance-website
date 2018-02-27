ba9caa9b62ec4719a7947bd28f2b38f3f37d4397
Ian Blais <me@ianblais.com>
2018-02-27 18:47:06 -0500

    Added Paypal Payments field to the admin page. Resolves #5

--- finance/admin.py
+++ finance/admin.py
@@ -10,7 +10,7 @@ class MonthlyDataAdmin(admin.ModelAdmin):
         (None,              {'fields': [('month','year')]}),
         ('Membership',      {'fields': ['members_cornerstone','members_full','members_fullwarden','members_student','members_studentwarden']}),
         ('Bank Account',    {'fields': ['pnc_beginning','pnc_deposits','pnc_deductions','pnc_ending']}),
-        ('Paypal Account',  {'fields': ['pp_beginning','pp_credits','pp_fees','pp_transfers','pp_ending']}),
+        ('Paypal Account',  {'fields': ['pp_beginning','pp_credits','pp_payments','pp_fees','pp_transfers','pp_ending']}),
         ('Incomes',   {'fields': ['otherincome','laserincome','otherexpense']}),
         ('Fixed Expenses',  {'fields': ['rent','cable','insurance'], 'classes': ['collapse']}),
     ]
